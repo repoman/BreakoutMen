c33b37e907d641ed404f10df586449a860126343
MegetLang <lassefredheim@gmail.com>
2016-04-16 13:17:21 +0200

    Nån endringa i ballan

--- BreakoutMen/Ball.cpp
+++ BreakoutMen/Ball.cpp
@@ -4,9 +4,23 @@
 
 Ball::Ball()
 {
+	xpos = 0;
+	ypos = 0;
+	xvel = 1;
+	yvel = 1;
 }
 
 
 Ball::~Ball()
+{}
+
+void Ball::Move(float tick)
 {
+	xpos += xvel*tick;
+	ypos += yvel*tick;
+}
+
+void Ball::render()
+{
+	
 }
--- BreakoutMen/Ball.h
+++ BreakoutMen/Ball.h
@@ -4,5 +4,13 @@ class Ball
 public:
 	Ball();
 	~Ball();
+	void Move(float tick);
+	void render();
+	static const int width = 10;
+	static const int height = 10;
+
+private:
+	float xpos, ypos, xvel, yvel;
+	
 };
 
